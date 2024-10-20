b7d030cc117939f723d0907ed9729b8591d5391a
Rawan <120096605+rwnmtwly@users.noreply.github.com>
2024-10-21 00:28:49 +0300

    mistake

--- project/ai_operations/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class TrainTestSplitSerializer(serializers.Serializer):
-    data = serializers.ListField(child=serializers.FloatField())
-    test_size = serializers.FloatField(default=0.2)
-    random_state = serializers.IntegerField(allow_null=True,required=False)
--- project/ai_operations/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import train_test_split_view
-
-
-urlpatterns = [
-    path('train-test-split/', train_test_split_view, name='train_test_split'),
-]
